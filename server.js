fb397d49bdf7f65a046e2c8371a4c397be487769
Mayan <mayan25sharma@gmail.com>
2025-09-25 01:32:13 +0530

    further components added

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
+// Serve the static files from the Vite build output directory
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // Handles any requests that don't match the ones above
-app.all('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 const port = process.env.PORT || 8080;
